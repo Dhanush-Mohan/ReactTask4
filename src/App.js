5cb664d8b501fdc446fb1e12cd83006a5af35c1f
Dhanush-Mohan <dhanish4777@gmail.com>
2023-10-15 00:44:19 +0530

    Upload files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import F1 from './components/F1';
+// import "../node_modules/axios/dist/axios";
+
+function App() {
+  return (
+    <div>
+      <F1/>
+    </div>
+  );
+}
+
+export default App;
